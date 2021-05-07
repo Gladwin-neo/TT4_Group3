48d5064c10d3f73eab4b367785b8214052a348b2
Tan Yuan Chuan <yuanchuantan@gmail.com>
2021-05-07 13:38:50 +0800

    added add Txn feature

--- dbs-ewallet/src/App.js
+++ dbs-ewallet/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import AddTxn from "./pages/AddTxn";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className=""
+      //style={{ backgroundImage: "url(/images/bg.jpg)App" }}
+    >
+      <BrowserRouter>
+        {/* <Route path="/" component={Home} /> */}
+        <Route path="/addTxn" component={AddTxn} />
+        {/* <Route path="/addTxn" render={() => <Register apiURL={apiURL} />} /> */}
+
+        {/* <Route exact path="/details/:id" render={(props)=>{<DetailsPage id={props.match.params.id}/>}} /> */}
+        {/* <Route path="/register" component={Register} /> */}
+        {/* <Route path="/footer" component={Footer} /> */}
+      </BrowserRouter>
     </div>
   );
 }
