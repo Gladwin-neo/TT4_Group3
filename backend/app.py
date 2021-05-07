a9ee1a4782ff0c397c5d3c1dd8729910a7bc0f14
Gladwin <gladwin.neo.2018@sis.smu.edu.sg>
2021-05-07 13:22:47 +0800

    view account balance

--- backend/app.py
+++ backend/app.py
@@ -37,6 +37,29 @@ def login():
 
     return response.text
 
+@app.route("/balance", methods=["POST"])
+def get_balance():
+    API_ENDPOINT = "https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/accounts"
+
+    get_account_balance = json.loads(request.data)
+    custID = get_account_balance['custID']
+    accountKey = get_account_balance['accountKey']
+
+    headers = {
+        'x-api-key': API_KEY,
+        'Content-Type': 'application/json'
+    }   
+    body = {
+        "custID": custID,
+        "accountKey" : accountKey
+    }
+
+    response = requests.request("POST", API_ENDPOINT, headers=headers, data=json.dumps(body))
+    if (response.status_code == 400):
+        return "Invalid customerId!"
+
+    return response.text
+
 @app.route("/transactions/view", methods=["POST"])
 def view_transactions():
     if not (request.method == "POST"):
